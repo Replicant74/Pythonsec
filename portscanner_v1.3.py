617e3e63664dbdaada7c81fe742ab6d1c99d5905
Replicant74 <32373728+Replicant74@users.noreply.github.com>
2018-04-17 23:38:43 +0100

    Tidied prior to branching.

--- portscanner_v1.3.py
+++ portscanner_v1.3.py
@@ -10,7 +10,6 @@ import time
 from Queue import Queue
 from datetime import datetime
 
-# Clear the screen
 subprocess.call('clear', shell=True)
 
 # Enter target host and port range
@@ -19,7 +18,7 @@ remoteServerIP  = socket.gethostbyname(remoteServer)
 startPort = int(raw_input("Enter the start port to scan: "))
 endPort = int(raw_input("Enter the end port to scan: "))
 
-# Print a banner with information on which host is being scanned
+# Banner displaying which host is being scanned
 print "-" * 60
 print "Please wait, scanning remote host...", remoteServerIP
 localtime = time.asctime(time.localtime())
@@ -29,10 +28,9 @@ print "-" * 60
 # Check what time the scan started
 t1 = datetime.now()
 
-# Using the range function to specify ports (here it will scans all ports between 1 and 1024)
+# Using the range function to specify ports (here it will scan all ports between 1 and 1024)
 
-# We also put in some error handling for catching errors
-
+# Error handling
 try:
     for port in range(startPort,endPort):  
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -57,9 +55,9 @@ except socket.error:
 # Checking the time again
 t2 = datetime.now()
 
-# Calculates the difference of time, to see how long it took to run the script
+# Calculates the difference in time, to see how long it took to run the script
 total =  t2 - t1
 print "-" * 60
 
-# Printing the information to screen
+# Display scan time taken
 print 'Scanning Completed in: ', total
