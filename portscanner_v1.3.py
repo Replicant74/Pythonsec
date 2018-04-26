f339a6a2afe648132f06e370645ee16bfda79363
Replicant74 <32373728+Replicant74@users.noreply.github.com>
2018-04-26 01:24:32 +0100

    v1.4

--- portscanner_v1.3.py
+++ portscanner_v1.3.py
@@ -8,27 +8,38 @@ from datetime import datetime
 
 subprocess.call('clear', shell=True)
 
+# print_lock = threading.Lock() - WIP, threading not implemented yet.
+
 # Enter target host and port range
-remoteServer    = input("Enter a remote host to scan: ")
-remoteServerIP  = socket.gethostbyname(remoteServer)
+target    = input("Enter a remote host to scan: ")
+targetIP  = socket.gethostbyname(target)
 startPort = int(input("Enter the start port to scan: "))
 endPort = int(input("Enter the end port to scan: "))
 
 # Banner displaying which host is being scanned
 print ("-" * 60)
-print ("Please wait, scanning remote host...", remoteServerIP)
+print ("Please wait, scanning remote host...", targetIP)
 localtime = time.asctime(time.localtime())
 print ("Scan started at: ", localtime)
+def checkhost(ip):
+    conf.verb = 0
+    try:
+        ping = sr1(IP(dst = ip)/ICMP())
+    except Exception:
+        print ("\n[!] Couldn't resolve target")
+        sys.exit("Exiting...")
 print ("-" * 60)
 
-# Check what time the scan started
+ports = range(int(startPort), int(endPort)+1)
 t1 = datetime.now()
+SYNACK = 0x12
+RSTACK = 0x14
 
 # Error handling
 try:
-    for port in range(startPort,endPort):  
+    for port in range(int(startPort), int(endPort)+1):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((remoteServerIP, port))
+        result = sock.connect_ex((targetIP, port))
         if result == 0:
             print ("Port {}: [+] Open".format(port))
         elif result != 0:
@@ -44,12 +55,9 @@ except socket.gaierror:
 except socket.error:
     sys.exit("Couldn't connect to server")
 
-# Checking the time again
 t2 = datetime.now()
 
 # Calculates the difference in time, to see how long it took to run the script
 total =  t2 - t1
 print ("-" * 60)
-
-# Display scan time taken
 print ("Scanning Completed in: ", total)
